a3ed81f89c2a967a512e92a5e8fc969e6f174f19
Róbert Suchý <robert.suchy5@gmail.com>
2023-05-08 12:43:59 +0200

    camunda

--- server.js
+++ server.js
@@ -1,104 +1,127 @@
 import express from "express"
 const app = express();
-import { Client, logger } from "camunda-external-task-client-js";
-import open from "open";
+import { Client, logger, Variables } from "camunda-external-task-client-js";
+import mysql from "mysql";
+import dateFormat from "dateformat";
 const config = { baseUrl: "http://localhost:8080/engine-rest", use: logger, asyncResponseTimeout: 10000 };
 const client = new Client(config);
 
-client.subscribe("transport-options", async function({ task, taskService }) {
-    const transports = [
-        {
-            "name" : "Osobný odber",
-            "price" : 0,
-            "icon" : "fas fa-hand-holding-usd",
-        },
-        {
-            "name" : "Doručenie na adresu",
-            "price" : 4.99,
-            "icon" : "fas fa-shipping-fast",
-        }
-    ]
+var db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    database: "sellphone"
+})
 
-    await taskService.complete(task, { variables: { transports } });
-});
+db.connect(function (err) {
+    if (err) throw err;
+    console.log("connected");
+})
 
-client.subscribe("payment-options", async function({ task, taskService }) {
+client.subscribe("customer-data", async function ({ task, taskService }) {
+    var userId = task.variables.get("user_id");
+    var name = task.variables.get("name");
+    var lastName = task.variables.get("last_name");
+    var email = task.variables.get("email");
+    var street = task.variables.get("street");
+    var streetNr = task.variables.get("street_nr");
+    var city = task.variables.get("city");
+    var psc = task.variables.get("psc");
+    var status = task.variables.get("status");
 
-    const payments = [
-        {
-            "name" : "Kartou online",
-            "price" : 0,
-            "icon" : "fab fa-cc-visa",
-        },
-        {
-            "name" : "Na dobierku",
-            "price" : 1.99,
-            "icon" : "fas fa-wallet",
-        }
-    ]
-    await taskService.complete(task, { variables: { payments } });
-});
-
-client.subscribe("user-data", async function({ task, taskService }) {
-    const users = {
-            "name" : "Michal",
-            "last_name" : "Mrkvička",
-            "email" : "mrkvicka.michal@gmail.com",
-            "street" : "Trakovická",
-            "street_nr" : "39",
-            "city" : "Piešťany",
-            "psc" : "988 09"
+    if (userId == null) {
+        userId = 0;
     }
-    await taskService.complete(task, { variables: { users } });
+
+    var date = new Date();
+    date = dateFormat(date, "yyyy-mm-dd HH:MM:ss");
+
+    var query = `INSERT INTO orders (user_id, name, last_name, email, street, street_nr, city, psc, status, created_at, updated_at) VALUES (${userId}, '${name}', '${lastName}', '${email}', '${street}', '${streetNr}', '${city}', '${psc}', '${status}', '${date}', '${date}')`;
+    var insertId;
+
+    if (name != null && lastName != null && email != null && street != null && streetNr != null && city != null && psc != null && status != null) {
+        db.query(query, function (error, result) {
+            if (error) throw error;
+            console.log("Customer data saved. Order ID: " + result.insertId);
+        });
+    }
+
+    await taskService.complete(task);
 });
 
-app.get("/transports", function(req, res) {
+client.subscribe("transport-options", async function ({ task, taskService }) {
+    const transport = task.variables.get("transport_name");
+    // if (transport != "Osobný odber" && transport != "Doručenie na adresu") {
+    //     throw "Transport method is not valid!"
+    // }
+
+    var query = `UPDATE orders\
+    SET transport_name = '${transport}' \
+    WHERE id = (\
+      SELECT id FROM orders\
+      ORDER BY created_at DESC\
+      LIMIT 1\
+    ))`;
+    db.query(query, function (error, result) {
+        console.log(result);
+    });
+
+    await taskService.complete(task, { variables: {} });
+});
+
+client.subscribe("payment-options", async function ({ task, taskService }) {
+    // console.log("payment");
+
+
+    await taskService.complete(task, { variables: {} });
+});
+
+app.get("/transports", function (req, res) {
     const transports = [
         {
-            "name" : "Osobný odber",
-            "price" : 0,
-            "icon" : "fas fa-hand-holding-usd",
+            "name": "Osobný odber",
+            "price": 0,
+            "icon": "fas fa-hand-holding-usd",
         },
         {
-            "name" : "Doručenie na adresu",
-            "price" : 4.99,
-            "icon" : "fas fa-shipping-fast",
+            "name": "Doručenie na adresu",
+            "price": 4.99,
+            "icon": "fas fa-shipping-fast",
         }
     ]
     res.status(200).send(transports);
 });
 
-app.get("/payments", function(req, res) {
+app.get("/payments", function (req, res) {
     const payments = [
         {
-            "name" : "Kartou online",
-            "price" : 0,
-            "icon" : "fab fa-cc-visa",
+            "name": "Kartou online",
+            "price": 0,
+            "icon": "fab fa-cc-visa",
         },
         {
-            "name" : "Na dobierku",
-            "price" : 1.99,
-            "icon" : "fas fa-wallet",
+            "name": "Na dobierku",
+            "price": 1.99,
+            "icon": "fas fa-wallet",
         }
     ]
     res.status(200).send(payments);
 });
 
-app.get("/user-details", function(req, res) {
+app.get("/user-details", function (req, res) {
     const users = [
         {
-            "name" : "Michal",
-            "last_name" : "Mrkvička",
-            "email" : "mrkvicka.michal@gmail.com",
-            "street" : "Trakovická",
-            "street_nr" : "39",
-            "city" : "Piešťany",
-            "psc" : "988 09"
+            "name": "Michal",
+            "last_name": "Mrkvička",
+            "email": "mrkvicka.michal@gmail.com",
+            "street": "Trakovická",
+            "street_nr": "39",
+            "city": "Piešťany",
+            "psc": "988 09"
         }
     ]
     res.status(200).send(users[0]);
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
     console.log("Microservice listening on port 3000");
 });
